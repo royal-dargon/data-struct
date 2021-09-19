61a4921d454f349a241456a4c11633c47f328ab8
royal-dargon <2312749625@qq.com>
2021-09-19 14:38:39 +0800

    null

---
+++ code/Link_list/code.cpp
@@ -0,0 +1,18 @@
+/*
+This is the code about circle linklist.
+The code was compiled by cpp.
+*/
+#include <iostream>
+
+using namespace std;
+
+class Link_List{
+    private:
+        int elem;
+        Link_List* next; 
+};
+
+int main()
+{
+    return 0;
+}
