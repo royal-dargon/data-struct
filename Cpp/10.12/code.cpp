132e7ee5d30f02e78a15a00bec3df667a18ec9bf
royal-dargon <2312749625@qq.com>
2021-10-12 15:32:35 +0800

    add:add the work in the class

---
+++ Cpp/10.12/code.cpp
@@ -0,0 +1,72 @@
+// 教材266 第七大题第二小题
+#include <iostream>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+struct Sub_score {
+    char* SubName;
+    double score;
+};
+
+class Student {
+    private:
+        int Id;
+        char* Name;
+        struct Sub_score Info[5];
+        // 用来存储记录的学科数
+        int count;
+    public:
+        Student(int id,char* name) {
+            Id = id;
+            int temp = strlen(name);
+            Name = new char[temp+1];
+            strcpy(Name,name);
+            count = 0;
+            //cout<<Id<<" "<<Name<<endl;
+        }
+        void Push_Sore(char* sname,double score);
+        void ShowInfo();
+        void get_average() {
+            int i;
+            double sum = 0;
+            for(i = 0;i < count;i ++) {
+                sum = sum + Info[i].score;
+            }
+            cout<<sum/count<<endl;
+        }
+};
+
+int main() 
+{
+    char Name[] = "张三";
+    Student stu1(10001,Name);
+    char Sub1[] = "语文";
+    char Sub2[] = "数学";
+    stu1.Push_Sore(Sub1,88);
+    stu1.Push_Sore(Sub2,89.5);
+    stu1.ShowInfo();
+    stu1.get_average();
+    return 0;
+}
+
+void Student::Push_Sore(char* sname,double scores) {
+    if(count >= 5) {
+        cout<<"all subjects has been used"<<endl;
+        return;
+    }
+    int lenth = strlen(sname);
+    Info[count].SubName = new char[lenth+1];
+    strcpy(Info[count].SubName,sname);
+    Info[count].score = scores;
+    count = count + 1;
+}
+
+void Student ::ShowInfo() {
+    cout<<Id<<" "<<Name<<endl;
+    int i;
+    for(i = 0;i < count;i ++) {
+        cout<<Info[i].SubName<<" "<<Info[i].score<<endl;
+    }
+}
---
+++ Cpp/10.12/code2.cpp
@@ -0,0 +1,117 @@
+// 教材第266页 第七大题第七小题
+#include <iostream>
+#include <string.h>
+#include <math.h>
+
+using namespace std;
+
+void getNext(char str[],int next[]);
+bool KMP(char str[],char tar[]);
+
+class String {
+    private:
+        char * Str;
+    public:
+        String(char *str) {
+            int lenth = strlen(str);
+            Str = new char[lenth+1];
+            strcpy(Str,str);
+            cout<<Str<<endl;
+        }
+        bool IsSubstring(char *str) {
+            bool res = KMP(Str,str);
+            cout<<res<<endl;
+            return res;
+        }
+        bool IsSubstring(const String & str) {
+            bool res = KMP(Str,str.Str);
+            cout<<res<<endl;
+            return res;
+        }
+        int str2num() {
+            int res = 0,i;
+            int lenth = strlen(Str);
+            int temp = lenth;
+            for(i = 0;i < lenth;i ++) {
+                res = (Str[i]-48)*pow(10,temp-1) + res;
+                temp --;
+            }
+            return res;
+        }
+        void toUppercase() {
+            int lenth = strlen(Str);
+            int i;
+            for(i = 0;i < lenth;i ++) {
+                if(Str[i] >= 'a' && Str[i] <= 'z') {
+                    Str[i] = Str[i] - 32;
+                }
+            }
+            cout<<Str<<endl;
+        }
+};
+
+int main() 
+{
+    char temp[] = "abccc";
+    char temp2[] = "12";
+    String str1(temp);
+    String str2(temp2);
+    char temp1[] = "cb";
+    str1.IsSubstring(temp1);
+    str1.IsSubstring(str2);
+    int res = str2.str2num();
+    cout<<res<<endl;
+    str1.toUppercase();
+    return 0;
+}
+
+void getNext(char str[],int next[]) {
+    int j = 0;
+    int i,k,m;
+    int flag;
+    for(i = 0;i < strlen(str);i ++) {
+        for(k = i;k > 0;k --) {
+            for(m = 0;m < k;m ++) {
+                if(str[m] != str[i+1-k+m])
+                    break;
+            }
+             if(m == k)
+            {
+                next[i] = k;
+                break;
+            }
+        }
+    }
+}
+
+bool KMP(char str[],char tar[])
+{
+    int i = 0;
+    int j = 0;
+    int lengh;
+    int next[100];
+    bool res = false;
+    lengh = strlen(tar);
+    memset(next,0,100*sizeof(int));
+    getNext(tar, next);
+    while(str[i] != '\0')
+    {
+        if(str[i] == tar[j])
+        {
+            i ++;
+            j ++;
+        } else {
+            if(j == 0)
+            {
+                i ++;
+            }
+            j = next[j];
+        }
+        if(j == lengh)
+        {
+            res = true;
+            return res;
+        }
+    }
+    return res;
+}
