169cd2b50a539be585bc3c56c900c133b292bc8b
royal-dargon <2312749625@qq.com>
2021-10-20 19:38:34 +0800

    add:add some files about cpp

--- Class_Code/10.13/code1.cpp
+++ Class_Code/10.13/code1.cpp
@@ -1,4 +1,9 @@
 // 实验三 书上378页实习一 人，货，厂，客
+// 设计思路程序每次启动，从一个txt文件中读取信息，然后创建一个链表
+// 分为管理者与购买者的模块
+// 在管理者方面有对货物添加的功能
+// 在购买者方面，每次的购买要自动对链表中的数据进行处理
+// 在关闭程序的时候又要向文件中写入本次更新后的信息，值得注意的是需要选择一个队文件中的数据进行完全清空的方式
 #include <iostream>
 #include <string.h>
 #include <string>
@@ -7,6 +12,7 @@
 using namespace std;
 
 struct Node {
+    int Id;
     string name;
     string model;
     double price;
@@ -26,56 +32,71 @@ class User {
         }
 };
 
-class Commodity {
-    public:
-        // 这种方法用于每次对文档的数据进行读入
-        Node* Creat();
-        // 这种方法用于每次的手动添加
-        void CreatByUser(Node* head);
-        void Show_commodity(Node *head);
-};
+// 这种方法用于每次对文档的数据进行读入
+Node* Creat();
+// 这种方法用于每次的手动添加
+void CreatByUser(Node* head);
+void Show_commodity(Node *head);
+// 通过这种方式在程序结束的时候向文件中写入本次链表的内容
+void Write_File(Node *head);
+// 购买者的函数，购买者通过查看链表的Id对商品进行购买
+void Buy_Things(Node *head);
 
 int main()
 {
-    Commodity tmp;
-    Node *head = tmp.Creat();
-    tmp.CreatByUser(head);
-    tmp.Show_commodity(head);
+    Node *head = Creat();
+    CreatByUser(head);
+    //cout << "!";
+    Show_commodity(head);
+    Write_File(head);
     return 0;
 }
 
 // 用来初始化链表的函数
-Node* Commodity::Creat() {
+// 同过文件的形式向文件进行读出
+Node* Creat() {
     Node *head,*pnew,*tail;
+    string fname,fmodel;
+    int fnum,fid;
+    double fprice;
+    //int fnum;
     head = new struct Node;
     head->next = NULL;
     tail = head;
-    int n = 2;
-    while(n) {
+    //int n = 2;
+    // 通过文件读的模式将文件进行打开
+    ifstream infile;
+    infile.open("shop.txt");
+    cout << "Reading from the file" << endl;
+    while(!infile.eof()) {
         pnew = new struct Node;
-        pnew->name = "冰箱";
-        pnew->model = "松下";
-        //int lenth1 = strlen(tmp1);
-        //int lenth2 = strlen(tmp2);
-        //pnew->name = new char[lenth1+1];
-        //pnew->model = new char[lenth2+1];
-        //strcpy(pnew->name,tmp1);
-        //strcpy(pnew->model,tmp2);
-        pnew->number = 100;
-        pnew->price = 200.5;
+        infile >> fid;
+        pnew->Id = fid;
+        infile >> fname;
+        pnew->name = fname;
+        //strcpy(pnew->name,fname);
+        infile >> fmodel;
+        pnew->model = fmodel;
+        //strcpy(pnew->model,fmodel);
+        infile >> fnum;
+        pnew->number = fnum;
+        infile >> fprice;
+        pnew->price = fprice;
         tail->next = pnew;
         pnew->next = NULL;
         tail = pnew;
-        n = n - 1;
     }
+    infile.close();
     return head;
 }
 
-void Commodity::Show_commodity(Node *head) {
+void Show_commodity(Node *head) {
     Node *p;
     p = head->next;
     while (p != NULL)
     {
+        cout<<"-------------------"<<endl;
+        cout<<"ID："<<p->Id<<endl;
         cout<<"商品名称:"<<p->name<<endl;
         cout<<"商品型号:"<<p->model<<endl;
         cout<<"商品价格:"<<p->price<<endl;
@@ -84,38 +105,62 @@ void Commodity::Show_commodity(Node *head) {
     }
 }
 
-void Commodity::CreatByUser(Node *head) {
+void CreatByUser(Node *head) {
     Node *pnew,*p,*tail;
-    p = head->next;
+    p = head;
+    int flag = 1;
+    int id;
     // 这一步用来将链表遍历到可以添加数据的地方
     while (p->next != NULL)
     {
         p = p->next;   
     }
     tail = p;
-    int n = 2;
+    id = p->Id;
+    cout<<id<<endl;
     cout<<"下面开始输入您要录入的信息"<<endl;
-    while(n) {   
-        string name;
-        string model;
-        int number;
-        double price;
+    while(flag) {   
+        string fname;
+        string fmodel;
+        int fnumber;
+        double fprice;
         cout<<"请输入商品的名字: ";
-        cin>>name;
+        cin>>fname;
         cout<<"请输入商品的型号: ";
-        cin>>model;
+        cin>>fmodel;
         cout<<"请输入商品的数量: ";
-        cin>>number;
+        cin>>fnumber;
         cout<<"请输入商品的单价：";
-        cin>>price;
+        cin>>fprice;
         pnew = new struct Node;
-        pnew->name = name;
-        pnew->model = model;
-        pnew->number = number;
-        pnew->price = price;
+        id ++;
+        pnew->Id = id;
+        pnew->name = fname;
+        pnew->model = fmodel;
+        pnew->number = fnumber;
+        pnew->price = fprice;
         pnew->next = NULL;
         tail->next = pnew;
         tail = pnew;
-        n = n -1;
+        cout<<"do you want to continue:(1/0) ";
+        cin >> flag;
     }
+}
+
+void Write_File(Node *head) {
+    // the mode to write
+    ofstream outfile;
+    outfile.open("shop.txt");
+    cout << "write to the file" << endl;
+    Node *p;
+    p = head->next;
+    while(p != NULL) {
+        outfile << p->Id << endl;
+        outfile << p->name << endl;
+        outfile << p->model << endl;
+        outfile << p->number << endl;
+        outfile << p->price << endl;
+        p = p->next;
+    }
+    outfile.close();
 }
