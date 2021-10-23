238a28dee70739b30605bf3af7d672400435762d
royal-dargon <2312749625@qq.com>
2021-10-23 21:46:34 +0800

    add:add some func about shopping

--- Class_Code/10.13/code1.cpp
+++ Class_Code/10.13/code1.cpp
@@ -20,7 +20,9 @@ struct Node {
     Node *next;
 };
 
+// 用来表示购物车的链表
 struct Buying {
+    int Id;
     string name;
     string model;
     double price;
@@ -268,6 +270,8 @@ Buying* Buy_Things(Node *head,double *sum) {
                 p->number = p->number - 1;
                 flag1 = 0;
                 pnew = new struct Buying;
+                // 添加了ID
+                pnew->Id = p->Id;
                 pnew->name = p->name;
                 pnew->model = p->model;
                 pnew->price = p->price;
@@ -310,8 +314,11 @@ void Delete_Things(Node *head) {
     int id;
     cin >> id;
     Node *p = head;
-    while(p->next != NULL) {
-        if(p->next->Id == id) {
+    // 在这里对while的条件进行了修改
+    // 同时也对结束的条件进行了修改
+    while(p != NULL) {
+        if(p->next->Id == id){
+            cout << "已经找到您要删除的商品的Id " << endl;
             break;
         }
         p = p->next;
@@ -319,13 +326,12 @@ void Delete_Things(Node *head) {
     if(p == NULL) {
         cout << "您输入的Id在当前系统中是不存在的! " << endl;
         return;
-    } else {
-        Node *temp,*q;
-        temp = p->next->next;
-        q = p->next;
-        p->next = temp;
-        delete q;
     }
+    Node *temp,*q;
+    temp = p->next->next;
+    q = p->next;
+    p->next = temp;
+    delete q;
     cout << "删除成功" << endl;
 }
 
@@ -337,16 +343,16 @@ void ChangeInfo(Node *head) {
     cout << "请选择您要修改的信息 1.商品名称 2.商品的型号 3.商品的价格 4.商品的数量 5.放弃修改" << endl;
     cin >> choice;
     if(choice == 1) {
-
+        Change_Things_Name(head);
     }
     else if(choice == 2) {
-
+        Change_Things_Model(head);
     }
     else if(choice == 3) {
         Change_Things_Price(head);
     }
     else if(choice == 4) {
-
+        Change_Things_Num(head);
     } else {
         cout << "成功放弃本次修改！" << endl;
     }
@@ -383,22 +389,97 @@ void Change_Things_Price(Node *head) {
 
 // 修改商品的名称
 void Change_Things_Name(Node *head) {
-
+    cout << "-------------现在是对商品的名称进行修改的界面-------------" << endl;
+    Show_commodity(head);
+    cout << "请输入您想要修改商品的ID ：" ;
+    int cid;
+    cin >> cid;
+    Node *p = head->next;
+    while (p != NULL)
+    {
+        if(p -> Id == cid) {
+            break;
+        }
+        p = p ->next;
+    }
+    if(p == NULL) {
+        cout << "您输入的Id在当前系统中是不存在的！" << endl;
+        return;
+    } 
+    cout << "您需要修改的商品的名称当前为： " << p->name << endl;
+    string str;
+    cout << "请输入您要修改为的名称 :";
+    cin >> str;
+    p -> name = str;
+    cout << "修改成功!" << endl;
 }
 
 // 修改商品的型号
 void Change_Things_Model(Node *head) {
-
+    cout  << "--------------现在是修改商品的型号的界面----------------" << endl;
+    Show_commodity(head);
+    cout << "请输入您想要修改的商品的ID：" ;
+    int cid;
+    Node *p = head -> next;
+    cin >> cid;
+    while (p != NULL)
+    {
+        if(p -> Id == cid) {
+            break;
+        }
+        p = p ->next;
+    }
+    if(p == NULL) {
+        cout << "您输入的Id在当前系统中是不存在的！" << endl;
+        return;
+    }
+    cout << "你想要修改的商品的型号是 " << p->model << endl;
+    cout << "请输入您想要修改为的型号 :";
+    string str;
+    cin >> str;
+    p -> model = str;
+    cout << "修改成功！" << endl; 
 }
 
 // 修改商品的数量
 void Change_Things_Num(Node *head) {
-
+    cout << "-----------------现在是修改商品的数量的界面--------------" << endl;
+    Show_commodity(head);
+    cout << "请输入您要修改的商品的Id ：" ;
+    int cid;
+    cin >> cid;
+    Node *p = head -> next;
+    while(p != NULL) {
+        if(p ->Id == cid) {
+            break;
+        }
+        p = p -> next;
+    }
+    if(p == NULL) {
+        cout << "您输入的商品在本系统是不存在的! " << endl;
+        return;
+    }
+    cout << "您需要修改的商品的当前数量是 ：" << p->number << endl;
+    int cnum;
+    cout << "请输入您需要修改为的数量: ";
+    cin >> cnum;
+    p ->number = cnum;
+    cout << "修改成功！" << endl;
 }
 
 // 查看购物车
 void Look_Buying(Buying *res) {
-
+    Buying *p;
+    p = res -> next;
+    while (p != NULL)
+    {
+        cout << "-----------------------" << endl;
+        cout << "商品名称 ："  << p->name << endl;
+        cout << "商品型号 ："  << p->model << endl;
+        cout << "商品的价格：" << p->price << endl;
+        p = p -> next;
+    }
+    cout << endl;
 }
 
 // 修改购物车
@@ -420,12 +501,114 @@ void Change_Buying(Buying *res,Node *head,double *sum) {
 
 // 对购物车内商品进行删除的操作
 void Delete_Buying(Buying *res,Node *head,double *sum) {
-
+    cout << "-------------------现在是对购物车中的商品进行删除的界面---------------" << endl;
+    Look_Buying(res);
+    string dname;
+    cout << "请输入您要删除的商品的名称" ;
+    cin >> dname;
+    Buying *p;
+    p = res->next;
+    // 定义一个flag来判断break的触发条件
+    int flag = 0;
+    // 记录删除的商品的id
+    int did;
+    // 记录删除的商品的价格
+    double dprice;
+    while(p != NULL) {
+        if(p -> next -> name == dname){
+            flag = 1;
+            break;
+        }
+        if(p -> name == dname) {
+            flag = 2;
+            break;
+        }
+        p = p -> next;
+    }
+    if(p == NULL) {
+        cout << "您输入的商品的名称错误或者在本系统中是不存在的 ！" << endl;
+        return;
+    }
+    cout << "您要删除的商品是 :" << p->name << endl;
+    cout << "正在删除中------" << endl; 
+    Buying *temp,*q;
+    if(flag == 1) {
+        temp = p ->next->next;
+        q = p->next;
+        p->next = temp;
+        did = q->Id;
+        dprice = q->price;
+        delete p;
+    }
+    // 进入这种情况就是说明删除的是购物车的第一个商品
+    if(flag == 2) {
+        temp = p->next;
+        q = p;
+        res->next = temp;
+        did = q -> Id;
+        dprice = q->price;
+        delete q;
+    }
+    cout << "已经成功删除，正在将商品放回货架上 -------" << endl;
+    Node *addNumber = head->next;
+    while(addNumber != NULL) {
+        if(addNumber ->Id == did) {
+            cout << "已经在货架上找到这个商品 " << endl;
+            break;
+        }
+        addNumber = addNumber -> next;
+    }
+    if(addNumber == NULL) {
+        cout << "这个商品已经不存在在货架上了" << endl;
+        return;
+    }
+    addNumber = addNumber + 1;
+    cout << "已经成功放回到货架上 ！" << endl;
+    *sum = *sum - dprice;
+    cout << endl;
 }
 
 // 用来继续添加购物车
 void Add_Buying(Buying *res,Node *head, double *sum) {
-
+    cout << "---------------尊敬的客户您现在在继续选购商品--------------" << endl;
+    Show_commodity(head);
+    cout << "请输入您想要继续购买的商品的ID :" << endl;
+    int add_Id;
+    cin >> add_Id;
+    Node *p;
+    p = head->next;
+    while(p != NULL) {
+        if(p->Id == add_Id) {
+            cout << "已找到您想要继续购买的商品 " << endl;
+            break;
+        }
+        p = p ->next;
+    }
+    if(p == NULL) {
+        cout << "您输入的商品的Id不存在! " << endl;
+        return;
+    }
+    if(p->number == 0) {
+        cout << "您想要购买的商品已经没有货了！" << endl;
+        return;
+    }
+    p->number = p->number - 1;
+    cout << "现在正在讲您想要购买的商品添加到购物车中-------" << endl;
+    Buying *q = res;
+    // 循环到购物车的末端
+    while(q->next != NULL) {
+        q = q->next;
+    }
+    Buying *pnew = new struct Buying;
+    pnew ->Id = p->Id;
+    pnew->name = p->name;
+    pnew->model = p->model;
+    pnew->price = pnew->price;
+    pnew->next = NULL;
+    q->next = pnew;
+    *sum = *sum + p->price;
+    cout << "添加完成 -----" << endl;
+    cout << endl;
 }
 
 
